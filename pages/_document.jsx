1d66cc57d1d8f2c8760a899ce3a672e33b5bfb74
LeThoumy <thomastamour@gmail.com>
2022-06-25 00:17:25 -0400

    Post-build

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,7 +7,7 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
           <style
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
